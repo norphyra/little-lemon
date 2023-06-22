4608adb069de015a9c25df3cca1c77c02b56f138
v.alex <av@akc.ru>
2023-06-22 21:26:37 +0600

    Delete imports is never used

--- app/src/main/java/com/example/littlelemonapp/Onboarding.kt
+++ app/src/main/java/com/example/littlelemonapp/Onboarding.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
--- app/src/main/java/com/example/littlelemonapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/littlelemonapp/ui/theme/Type.kt
@@ -1,11 +1,9 @@
 package com.example.littlelemonapp.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.Paragraph
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.littlelemonapp.R
